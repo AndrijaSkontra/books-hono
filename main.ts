1e0879868ca5b01597a70876996fbd694e8db59b
AndrijaSkontra <andrija.skontra@kodelab.hr>
2025-02-28 08:20:40 +0100

    bootstrap and loaders

--- main.ts
+++ main.ts
@@ -1,9 +1,30 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
+import { html } from "hono/html";
+import { serveStatic } from "hono/deno";
+import indexHtml from "./views/index.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+//middleware to serve all static files in the /public dir
+app.use("/public/*", serveStatic({ root: "./" }));
+
+app.get("/", (c: Context) => {
+  return c.html(indexHtml());
+});
+
+app.post("/books", async (c: Context) => {
+  const formData = await c.req.formData();
+  const book = formData.get("book") as string;
+  await sleep(1000);
+  if (book) {
+    return c.html(html` <li>${book}</li> `);
+  } else {
+    return new Response("Invalid book", { status: 400 });
+  }
 });
 
 Deno.serve(app.fetch);
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
---
+++ views/index.ts
@@ -0,0 +1,65 @@
+import { html } from "hono/html";
+
+export default function indexHtml() {
+  return html`
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script
+          src="https://unpkg.com/htmx.org@2.0.4"
+          integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+"
+          crossorigin="anonymous"
+        ></script>
+        <link rel="stylesheet" href="/public/styles.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous"
+        />
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+          crossorigin="anonymous"
+        ></script>
+        <title>Shkoki books</title>
+      </head>
+      <body hx-boost="true">
+        <form hx-on::after-request="this.reset()">
+          <input
+            type="text"
+            name="book"
+            placeholder="Book name"
+            class="w-25 form-control"
+            style="background-color: #eee;"
+          />
+          <button
+            type="submit"
+            hx-post="/books"
+            hx-target="#books-list"
+            hx-swap="beforeend"
+            class="btn btn-primary"
+          >
+            Add
+            <img class="htmx-indicator" src="/public/spinning-circles.svg" />
+          </button>
+        </form>
+        <ul id="books-list">
+          <li>First book</li>
+        </ul>
+      </body>
+    </html>
+  `;
+}
